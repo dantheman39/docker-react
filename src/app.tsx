98ee9bc79aaa8cb6e4a9143aebdaa0a6a2a85c91
Daniel Riggs <daniel.riggs1@gmail.com>
2021-01-08 11:06:04 -0500

    Making a change to see if travis will run

--- src/app.tsx
+++ src/app.tsx
@@ -3,9 +3,7 @@ import React from "react";
 export const App = () => {
   return (
     <div>
-      Hi! What's goin on? HMR isn't working with docker-compose, but with docker it does work.
-      Works even less well on my linux machine. Boooo. Wait, I figured some of it out
-
+      Hi! What's goin on?
     </div>
   );
 };
